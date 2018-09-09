5c31be1376d8ae4ad05e2e22c4f4c65454ada690
Bob Tanski <btanski@gmail.com>
2018-09-09 11:54:11 -0400

    Newest

--- crimeData.go
+++ crimeData.go
@@ -5,8 +5,6 @@ import (
 	"encoding/csv"
 	"encoding/json"
 
-	//"encoding/json"
-
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -17,6 +15,7 @@ import (
 	"github.com/go-martini/martini"
 )
 
+//CrimeDataEntry is the structure for each row of Crime Data
 type CrimeDataEntry struct {
 	ID                 int
 	IncidentNumber     string
@@ -38,16 +37,19 @@ type CrimeDataEntry struct {
 	Location           string
 }
 
+//CrimeData is the slice array used to hold all of the Crime Data
 type CrimeData struct {
 	CrimeDataBook []*CrimeDataEntry
 }
 
+//NewCrimeDataBook creates the empty CrimeDataBook Slice
 func NewCrimeDataBook() *CrimeData {
 	return &CrimeData{
 		make([]*CrimeDataEntry, 0),
 	}
 }
 
+//WebService interfaces
 type WebService interface {
 	GetPath() string
 	WebDelete(params martini.Params) (int, string)
@@ -55,11 +57,13 @@ type WebService interface {
 	WebPost(params martini.Params, req *http.Request) (int, string)
 }
 
-func (c CrimeData) GetPath() string {
+//GetPath is the implmentation of the GetPath Interface
+func (c *CrimeData) GetPath() string {
 	return "/crimebook"
 }
 
-func (c CrimeData) WebDelete(params martini.Params) (int, string) {
+//WebDelete is the implmentation of the WebDelete Interface
+func (c *CrimeData) WebDelete(params martini.Params) (int, string) {
 	if len(params) == 0 {
 		c.RemoveAllEntries()
 		return http.StatusOK, "collection deleted"
@@ -78,10 +82,8 @@ func (c CrimeData) WebDelete(params martini.Params) (int, string) {
 	return http.StatusOK, "entry deleted"
 }
 
-func (c CrimeData) WebGet(params martini.Params) (int, string) {
-	/*fmt.Println(len(params))
-	fmt.Println(params)
-	return http.StatusOK, "OK Get"*/
+//WebGet is the implementation of the WebGet interface
+func (c *CrimeData) WebGet(params martini.Params) (int, string) {
 	if len(params) == 0 {
 		jsonResults, err := json.Marshal(c.GetAllEntries())
 		if err != nil {
@@ -104,7 +106,8 @@ func (c CrimeData) WebGet(params martini.Params) (int, string) {
 	return http.StatusOK, string(jsonEntry)
 }
 
-func (c CrimeData) WebPost(params martini.Params, req *http.Request) (int, string) {
+//WebPost is the implementation of the WebPost Interface
+func (c *CrimeData) WebPost(params martini.Params, req *http.Request) (int, string) {
 	defer req.Body.Close()
 
 	requestBody, err := ioutil.ReadAll(req.Body)
@@ -129,34 +132,16 @@ func (c CrimeData) WebPost(params martini.Params, req *http.Request) (int, strin
 	fmt.Println()
 	fmt.Println(crimeDataEntry)
 
-	/*newEntry := &CrimeDataEntry{
-		ID,
-		line[0],
-		line[1],
-		line[2],
-		line[3],
-		line[4],
-		line[5],
-		line[6],
-		line[7],
-		line[8],
-		line[9],
-		line[10],
-		line[11],
-		line[12],
-		line[13],
-		line[14],
-		line[15],
-		line[16],
-	}*/
-
 	fmt.Println(len(c.CrimeDataBook))
 	c.CrimeDataBook = append(c.CrimeDataBook, &crimeDataEntry)
 	fmt.Println(len(c.CrimeDataBook))
+	fmt.Printf("%T %v\n", crimeDataEntry.ID, crimeDataEntry.ID)
 
-	return http.StatusOK, "OK Post"
+	return http.StatusOK, "new entry created"
 }
-func (c CrimeData) GetAllEntries() []*CrimeDataEntry {
+
+//GetAllEntries is used by WebGet to get all the entries
+func (c *CrimeData) GetAllEntries() []*CrimeDataEntry {
 	entries := make([]*CrimeDataEntry, 0)
 	for _, entry := range c.CrimeDataBook {
 		if entry != nil {
@@ -166,20 +151,23 @@ func (c CrimeData) GetAllEntries() []*CrimeDataEntry {
 	return entries
 }
 
-func (c CrimeData) GetEntry(id int) (*CrimeDataEntry, error) {
+//GetEntry is used be WebGet to get a single Entry
+func (c *CrimeData) GetEntry(id int) (*CrimeDataEntry, error) {
 	if id < 0 || id >= len(c.CrimeDataBook) {
 		return nil, fmt.Errorf("invalid id")
 	}
 	return c.CrimeDataBook[id], nil
 }
 
-func (c CrimeData) RemoveAllEntries() {
+//RemoveAllEntries is used by WebDelete to delete all the entries
+func (c *CrimeData) RemoveAllEntries() {
 	fmt.Println(len(c.CrimeDataBook))
 	c.CrimeDataBook = []*CrimeDataEntry{}
 	fmt.Println(len(c.CrimeDataBook))
 }
 
-func (c CrimeData) RemoveEntry(id int) error {
+//RemoveEntry is used by WebDelete to remove a single entry
+func (c *CrimeData) RemoveEntry(id int) error {
 	if id < 0 || id >= len(c.CrimeDataBook) {
 		return fmt.Errorf("invalid id")
 	}
@@ -189,6 +177,7 @@ func (c CrimeData) RemoveEntry(id int) error {
 	return nil
 }
 
+//RegisterWebService is used to register the web services
 func RegisterWebService(webService WebService, classicMartini *martini.ClassicMartini) {
 	path := webService.GetPath()
 
@@ -237,6 +226,7 @@ func checkError(err error) {
 	}
 }
 
+//AddEntry is used to add an entry to the Crime Data Book
 func (c *CrimeData) AddEntry(line []string) {
 	ID := len(c.CrimeDataBook)
 
